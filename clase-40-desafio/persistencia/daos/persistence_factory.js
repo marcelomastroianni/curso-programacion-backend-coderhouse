86ee1c132b04aae9acdaca0b4bee0cccda6428cc
marcelomastroianni <marcelomastroianni@users.noreply.github.com>
2023-03-05 18:00:59 +0000

    clase-40-desafio

--- clase-40-desafio/persistencia/daos/persistence_factory.js
+++ clase-40-desafio/persistencia/daos/persistence_factory.js
@@ -3,28 +3,45 @@ const config = require('../../config/config.js');
 
 
  class PersistenceFactory {
+
+    static usuariosDao = null;
+    static mensajesDao = null;
+
+
     static getPersistence = async(entidad) => {
         if (entidad == "usuarios") {
             switch(config.PERSISTENCE) {
                 case "MONGODB":{
-                    const UsuariosDaoMongo =  require('./usuarios_dao_mongodb.js');
-                    return new UsuariosDaoMongo()
+                    if (this.usuariosDao == null) {
+                        const UsuariosDaoMongo =  require('./usuarios_dao_mongodb.js');
+                        this.usuariosDao = new UsuariosDaoMongo();
+                    }
+                    return this.usuariosDao;
                 }
                 case "FILE":{
-                    const UsuariosDaoArchivo =  require('./usuarios_dao_archivo.js');
-                    return new UsuariosDaoArchivo()
+                    if (this.usuariosDao == null) {
+                        const UsuariosDaoArchivo =  require('./usuarios_dao_archivo.js');
+                        this.usuariosDao = new UsuariosDaoArchivo();
+                    }
+                    return this.usuariosDao;
                 }
             }
         }
         else if (entidad == "mensajes") {
             switch(config.PERSISTENCE) {
                 case "MONGODB":{
-                    const MensajesDaoMongo =  require('./mensajes_dao_mongodb.js');
-                    return new MensajesDaoMongo()
+                    if (this.mensajesDao == null) {
+                        const MensajesDaoMongo =  require('./mensajes_dao_mongodb.js');
+                        this.mensajesDao = new MensajesDaoMongo();
+                    }
+                    return this.mensajesDao;
                 }
                 case "FILE":{
-                    const MensajesDaoArchivo =  require('./mensajes_dao_archivo.js');
-                    return new MensajesDaoArchivo()
+                    if (this.mensajesDao == null) {
+                        const MensajesDaoArchivo =  require('./mensajes_dao_archivo.js');
+                        this.mensajesDao = new MensajesDaoArchivo();
+                    }
+                    return this.mensajesDao;
                 }
             }
         }
