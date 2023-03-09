5c1da475b1cdc3415565404fa96ee8c100b38b60
Marcelo Mastroianni <marcelomastroianni@gmail.com>
2023-03-09 19:47:16 -0300

    clase-36-desafio-tp-3

--- clase-36-desafio-tp-3/public/index.js
+++ clase-36-desafio-tp-3/public/index.js
@@ -74,9 +74,49 @@
       }
 
       
+      async function getData(url = '') {
+        const response = await fetch(url, {
+          method: 'GET',
+          mode: 'cors', 
+          cache: 'no-cache',
+          credentials: 'same-origin', 
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          redirect: 'follow', 
+          referrerPolicy: 'no-referrer'
+        });
+        return response.json();
+      }
+
       
       
 
+  //Se consulta el usuario logueado, en caso de no estar logueado se redirecciona al login
+  getData("api/users/profile")
+  .then((data) => {
+    //let divWelcomeMessage = document.getElementById('divWelcomeMessage');
+    //let txtWelcomeMessage = document.getElementById('txtWelcomeMessage');
+    if (data.body.username){
+      //divWelcomeMessage.style.display = "block";
+      //txtWelcomeMessage.innerHTML = `Bienvenido ${data.body.username}`;
+      alert(`Bienvenido ${data.body.username}`);
+
+      //let btnLogout = document.getElementById('btnLogout');
+      //btnLogout.addEventListener('click', function(e) {
+      //  e.preventDefault();
+      //  postData('/api/users/logout', {})
+      //  .then((data_logout) => {
+      //    window.location.href = "/logout.html?username="+data.body.username;
+      //  });
+      //});
+
+
+    }else{
+      //divWelcomeMessage.style.display = "none";
+      window.location.href = "/login.html";
+    }
   
+  });
 
      
