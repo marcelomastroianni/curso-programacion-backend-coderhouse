a3ec0d45261687500f578628d34a6a26df44c6b6
Marcelo Mastroianni <marcelomastroianni@gmail.com>
2023-03-14 19:04:26 -0300

    clase-36-desafio

--- clase-36-desafio-tp-3/middlewares/security.middleware.js
+++ clase-36-desafio-tp-3/middlewares/security.middleware.js
@@ -1,6 +1,17 @@
-const validateIfAdmin = (req, res, next) => {
-    const { is_admin } = req.headers;
-    if (is_admin !== "true") {
+const DaoFactory = require('../daos');
+const usersDao = DaoFactory.getDao('usuarios')
+
+
+const validateIfAdmin = async (req, res, next) => {
+
+    const user_uuid  = req.session.passport.user;
+
+    const user = await usersDao.getById(user_uuid);
+
+    const is_admin = user.is_admin;
+
+    //const { is_admin } = req.headers;
+    if (is_admin !== true) {
       let full_path = req.baseUrl+req.route.path;
       let method = req.method;
       let message = `ruta ${full_path} método ${method} no autorizada`;
