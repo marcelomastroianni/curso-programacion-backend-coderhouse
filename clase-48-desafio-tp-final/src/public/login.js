e39046e535386878cd9fbb6b55548a6f08b9cd26
Marcelo Mastroianni <marcelomastroianni@gmail.com>
2023-04-23 11:41:57 -0300

    clase-48-desafio-tp-final

--- clase-48-desafio-tp-final/src/public/login.js
+++ clase-48-desafio-tp-final/src/public/login.js
@@ -1,5 +1,27 @@
 
 
+
+ function validateLoginForm(){
+
+    const username = document.getElementById('txtNombre').value;
+
+    if (username.length < 3){
+        alert("Por favor ingrese nombre de usuario");
+        return false;
+    }
+
+    const password = document.getElementById('txtPassword').value;
+
+    if (password.length < 3){
+        alert("Por favor ingrese la contraseña");
+        return false;
+    }
+
+    return true;
+ }
+
+
+
   let login_form = document.getElementById('login_form');
 
     login_form.addEventListener('submit', async (e) => {
@@ -7,53 +29,57 @@
         const username = document.getElementById('txtNombre').value;
         const password = document.getElementById('txtPassword').value;
 
-        let response = null;
+        if (validateLoginForm()){
+            
+            let response = null;
 
-        try{
-            response = await fetch('/api/users/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username,password })
-            });
+            try{
+                response = await fetch('/api/users/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ username,password })
+                });
 
-            if (response.redirected) {
-                window.location.href = response.url;
-            }else{
-                const data = await response.json();
-    
-    
-                const access_token = data.access_token;
-                window.sessionStorage.setItem("access_token", access_token);
-                console.log("access_token", access_token);
-                
-                console.log("data", data);
-
-                if (data.statusCode === 401){
-                    window.location.href = '/loginfail.html'
-
+                if (response.redirected) {
+                    window.location.href = response.url;
                 }else{
-                    window.location.href = '/';
-                }
-
-                //window.location.href = '/';
-    
-    
-                /*
-                if (data.status === 'ok') {
-                    window.location.href = '/';
-                }
-                else {
-                    alert(data.body.error);
-                }*/
-            }
-        }
-        catch(error){
-            console.log(error);
-            window.location.href = '/loginfail.html'
-        }
+                    const data = await response.json();
         
+        
+                    const access_token = data.access_token;
+                    window.sessionStorage.setItem("access_token", access_token);
+                    console.log("access_token", access_token);
+                    
+                    console.log("data", data);
+
+                    if (data.statusCode === 401){
+                        window.location.href = '/loginfail.html'
+
+                    }else{
+                        window.location.href = '/';
+                    }
+
+                    //window.location.href = '/';
+        
+        
+                    /*
+                    if (data.status === 'ok') {
+                        window.location.href = '/';
+                    }
+                    else {
+                        alert(data.body.error);
+                    }*/
+                }
+            }
+            catch(error){
+                console.log(error);
+                window.location.href = '/loginfail.html'
+            }
+    
+        }
+                
 
      
 
